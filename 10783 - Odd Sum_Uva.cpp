4f07c1eda2cf721f5d8c3173939e0aa3f1971529
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2018-10-10 22:58:15 +0600

    Add files via upload

---
+++ 10783 - Odd Sum_Uva.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+
+    cin>>t;
+    int sum=0;
+    for(int i=1;i<=t;i++){
+        int a,b;
+        cin>>a>>b;
+        sum=0;
+        for(int j = a;j<=b;j++){
+            if(j%2!=0){
+             sum=sum+j;
+            }
+
+        }
+        cout<<"Case "<<i<<": "<<sum<<endl;
+    }
+
+
+
+
+    return 0;
+}
