d407a6f8f380f0689829ac19e03ca5e8a0a935b1
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2018-10-28 11:24:10 +0600

    Add files via upload

---
+++ Birthday Party_hackerearth.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t,n;
+    long long m;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>n>>m;
+        if(m%n==0)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+
+    return 0;
+}
+
