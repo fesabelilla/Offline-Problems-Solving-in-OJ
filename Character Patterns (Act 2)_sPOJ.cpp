69f23beb3ccdffd014e3162d8c53b7ac66a140ae
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2019-01-14 08:52:23 +0600

    Add files via upload

---
+++ Character Patterns (Act 2)_sPOJ.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n;
+
+    cin>>m>>n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=m; j++)
+        {
+            cout<<"*";
+            cout<<"\n";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
